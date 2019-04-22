5d1e7ce6437652aa55a924fb8282ff121a0b7190
Oliver Gomes <ogomesphoto@gmail.com>
2019-04-22 14:39:08 -0400

    easy jump links update

--- javascript/jump-links-easy.js
+++ javascript/jump-links-easy.js
@@ -1,10 +1,8 @@
 // Use Case:
 // used for jumping links that works with url parameters esp. on unbounce
 
-<script>
-  let agaanchorlinks = document.querySelectorAll('a[href^="#"]') ; for (let link
-  of agaanchorlinks)
-  {link.addEventListener("click", e => {
+for (let link of agaanchorlinks) {
+  link.addEventListener("click", e => {
     let hashval = link.getAttribute("href");
     let target = document.querySelector(hashval);
     target.scrollIntoView({
@@ -12,5 +10,5 @@
     });
     history.pushState(null, null, hashval);
     e.preventDefault();
-  })}
-</script>;
+  });
+}

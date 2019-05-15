1d9e9f590c21cd21bce352bf7c96b03305ea2d1c
Oliver Gomes <ogomesphoto@gmail.com>
2019-05-15 14:38:09 -0400

    Feather add

---
+++ javascript/feather.js
@@ -0,0 +1,25 @@
+!(function(f, e, a, t, h, r) {
+  if (!f[h]) {
+    (r = f[h] = function() {
+      r.invoke ? r.invoke.apply(r, arguments) : r.queue.push(arguments);
+    }),
+      (r.queue = []),
+      (r.loaded = 1 * new Date()),
+      (r.version = "1.0.0"),
+      (f.FeathrBoomerang = r);
+    var g = e.createElement(a),
+      h =
+        e.getElementsByTagName("head")[0] ||
+        e.getElementsByTagName("script")[0].parentNode;
+    (g.async = !0), (g.src = t), h.appendChild(g);
+  }
+})(
+  window,
+  document,
+  "script",
+  "https://cdn.feathr.co/js/boomerang.min.js",
+  "feathr"
+);
+
+feathr("fly", "5818fc487c1fea1278ec944e");
+feathr("sprinkle", "page_view");
